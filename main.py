e865aedb77f14592e56fcf4f4452e413370132a4
Yasiru Perera <yasiruperera682@gmail.com>
2023-04-10 23:03:03 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+#this two linesof code will keep open the browser after the prosess happend
+options = Options()
+options.add_experimental_option( "detach" , True)
+
+driver_manager = ChromeDriverManager()
+driver = webdriver.Chrome(service=Service(driver_manager.install()), options=options)
+
+
+
+
+driver.get("https://www.neuralnine.com")
+driver.maximize_window()
